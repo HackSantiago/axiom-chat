e5c2ac463a4ea97b8f1b67321a1e1355b497aa9c
p1nox <pinox101@gmail.com>
2015-09-24 12:00:53 -0300

    remove references to contact page

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var assets = require('connect-assets');
 var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
-var contactController = require('./controllers/contact');
+
 
 /**
  * API keys and Passport configuration.
@@ -103,8 +103,6 @@ app.get('/reset/:token', userController.getReset);
 app.post('/reset/:token', userController.postReset);
 app.get('/signup', userController.getSignup);
 app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
 app.get('/account', passportConf.isAuthenticated, userController.getAccount);
 app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
