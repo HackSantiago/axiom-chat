32c79d60406b07c1fccffbb13f0222e39618abb0
p1nox <pinox101@gmail.com>
2015-09-24 12:25:14 -0300

    implement socketio in app routes, controller action added to handle socket connections and socketio installed

--- controllers/chat.js
+++ controllers/chat.js
@@ -2,3 +2,17 @@
 exports.index = function(req, res) {
   res.render('chat');
 };
+
+exports.handleSocketConnection = function(io) {
+
+  io.sockets.on('connection', function (socket) {
+
+    socket.emit('message', { text: 'Welcome to the Volatile AxiomChat!', user_name: 'VolatileBot' });
+
+    socket.on('send', function (data) {
+      io.sockets.emit('message', data);
+    });
+
+  });
+
+}
